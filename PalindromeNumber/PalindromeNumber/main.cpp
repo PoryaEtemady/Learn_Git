1af545c19f10c67a0b1433fa84f9219b0dcdabc5
porya <poryaetemady72@gmail.com>
2024-04-11 18:35:34 +0330

    add two solution to PalindromeNumber problem for enhance speed

--- PalindromeNumber/PalindromeNumber/main.cpp
+++ PalindromeNumber/PalindromeNumber/main.cpp
@@ -2,7 +2,15 @@
 #include "qlogging.h"
 #include <vector>
 #include <cmath> // for log10 function
+
+#define SOL1    0//first try
+#define SOL2    1//enhanced solution
+#define SOL3    2//FASTEST WAY
+
+#define SOLUTION  SOL3
+
 using namespace std;
+#if SOLUTION==SOL1
 class Solution {
 public:
     bool isPalindrome(int x)
@@ -31,13 +39,54 @@ public:
         }
     }
 };
+#elif SOLUTION==SOL2
+class Solution {
+public:
+    bool isPalindrome(int x)
+    {
+        int input_num = x;
+        if(input_num<0)
+            return false;
+        else
+        {
+            if(input_num==0)
+                return true;
+            long reverse=0;
+            while(input_num!=0)
+            {
+                reverse = reverse*10+input_num%10;
+                input_num = input_num/10;
+            }
 
+            if(reverse == x)
+                return true;
+            else
+                return false;
+        }
+    }
+};
+#elif SOLUTION==SOL3
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string s = to_string(x);
+        for (long long i = 0; i < s.length(); i++)
+        {
+            if (s[i] != s[s.length() - i - 1])
+                return false;
+        }
+        return true;
+    }
+};
+#endif
 
 
 int main(void)
 {
     Solution solution;
-    bool val = solution.isPalindrome(1234321);
+    bool val = solution.isPalindrome(123456789);
     qDebug()<<"Value = "<<val;
     return 0;
 }
